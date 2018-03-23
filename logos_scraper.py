3b4c0784e64f659a4245e207273825c266d0a7c8
milosmladenovic5 <milosml@outlook.com>
2018-03-23 22:09:29 +0100

    applying implementation to all football clubs

--- logos_scraper.py
+++ logos_scraper.py
@@ -1,15 +1,52 @@
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as  uReq
 import requests
+import urllib.parse
 
 urls = [#'https://en.wikipedia.org/wiki/Category:English_football_logos',
         #'https://en.wikipedia.org/w/index.php?title=Category:English_football_logos&filefrom=Chessington+%26+Hook+United+F.C.+logo.png#mw-category-media'
         #'https://en.wikipedia.org/w/index.php?title=Category:English_football_logos&filefrom=Hayling+United+logo.png#mw-category-media',
         #'https://en.wikipedia.org/w/index.php?title=Category:English_football_logos&filefrom=Norton-United-Logo.png#mw-category-media',
-        'https://en.wikipedia.org/w/index.php?title=Category:English_football_logos&filefrom=Swindon+town+fc+badge+1990.PNG'
+        #'https://en.wikipedia.org/w/index.php?title=Category:English_football_logos&filefrom=Swindon+town+fc+badge+1990.PNG'
+        #'https://en.wikipedia.org/wiki/Category:Australian_soccer_logos',
+        #'https://en.wikipedia.org/w/index.php?title=Category:Australian_soccer_logos&filefrom=South+Hobart+FC.png#mw-category-media'
+        #'https://en.wikipedia.org/wiki/Category:Welsh_football_logos',
+        #'https://en.wikipedia.org/wiki/Category:Scottish_football_logos',
+        #'https://en.wikipedia.org/wiki/Category:Spanish_football_logos',
+        #'https://en.wikipedia.org/w/index.php?title=Category:Spanish_football_logos&filefrom=CD+Masnou.png',#mw-category-media
+        #'https://en.wikipedia.org/w/index.php?title=Category:Spanish_football_logos&filefrom=Lleida+Esportiu.svg',
+        #'https://en.wikipedia.org/w/index.php?title=Category:Spanish_football_logos&filefrom=UE+Lleida+escudo.png#mw-category-media',
+        #'https://en.wikipedia.org/wiki/Category:Northern_Irish_football_logos',
+        #'https://en.wikipedia.org/wiki/Category:German_football_logos',
+        #'https://en.wikipedia.org/w/index.php?title=Category:German_football_logos&filefrom=Danzig%2C+BuEV%0ABuEV+Danzig.png#mw-category-media'
+        #'https://en.wikipedia.org/w/index.php?title=Category:German_football_logos&filefrom=Kastel%2C+FVgg%0AFVgg+Kastel.png#mw-category-media',
+        #'https://en.wikipedia.org/w/index.php?title=Category:German_football_logos&filefrom=SpVgg+05+Bad+Homburg.gif#mw-category-media'
+        #'https://en.wikipedia.org/wiki/Category:Italian_football_logos',
+        #'https://en.wikipedia.org/w/index.php?title=Category:Italian_football_logos&filefrom=Modena+FC+logo.svg',
+        #'https://en.wikipedia.org/wiki/Category:French_football_logos',
+        #'https://en.wikipedia.org/wiki/Category:Portuguese_football_logos',
+        #'https://en.wikipedia.org/w/index.php?title=Category:Brazilian_football_logos&filefrom=Real+Desportivo+Ariquemes+Futebol+Clube.png#mw-category-media',
+        #'https://en.wikipedia.org/wiki/Category:Czech_football_logos',
+        #'https://en.wikipedia.org/wiki/Category:Dutch_football_logos',
+        #'https://en.wikipedia.org/wiki/Category:American_soccer_logos',
+        #'https://en.wikipedia.org/wiki/Category:Russian_football_logos',
+        #'https://en.wikipedia.org/wiki/Category:Mexican_football_logos',
+        #'https://en.wikipedia.org/wiki/Category:Norwegian_football_logos',
+        #'https://en.wikipedia.org/wiki/Category:Swedish_football_logos',
+        #'https://en.wikipedia.org/wiki/Category:Slovak_football_logos',
+        #'https://en.wikipedia.org/wiki/Category:Slovenian_football_logos',
+        #'https://en.wikipedia.org/wiki/Category:Swiss_football_logos',
+        #'https://en.wikipedia.org/wiki/Category:Polish_football_logos',
+        #'https://en.wikipedia.org/wiki/Category:Serbian_football_logos',
+        #'https://en.wikipedia.org/wiki/Category:Thai_football_logos',
+        #'https://en.wikipedia.org/wiki/Category:Peruvian_football_logos',
+        'https://en.wikipedia.org/wiki/Category:Greek_football_logos'
         ]
 
 wiki = "https://en.m.wikipedia.org/wiki/"
+
+
+print("itsok")
 
 def get_club_name(string):
     name = string[5:len(string)]
@@ -20,6 +57,7 @@ def get_club_name(string):
 
 for my_url in urls:
     all_links = []
+    #data = data.encode('utf-8') # data should be bytes
     https = "https:"
     uClient = uReq(my_url)
     page_html = uClient.read()
@@ -33,29 +71,17 @@ for my_url in urls:
         new_link = "" + wiki + link.text
         new_link = new_link.replace(" ", "_")
         all_links.append(new_link)
-        
-    #Removing invalid links, those that can't be opened 
-    if my_url == 'https://en.wikipedia.org/wiki/Category:English_football_logos':    
-        all_links.pop(3)
-        all_links.pop(65)
-        all_links.pop(128)
-        all_links.pop(129)
-        all_links.pop(148)
-    elif my_url == "https://en.wikipedia.org/w/index.php?title=Category:English_football_logos&filefrom=Chessington+%26+Hook+United+F.C.+logo.png#mw-category-media" : 
-        print("Here I am")
-        all_links.pop(109)
-        all_links.pop(110)     
-        all_links.pop(111)
-        all_links.pop(112)
-    elif my_url == 'https://en.wikipedia.org/w/index.php?title=Category:English_football_logos&filefrom=Swindon+town+fc+badge+1990.PNG':
-        #fourth link contains invalid link   
-        all_links.pop(1)
-        print("We will remove something else.")
-    #Checking if a link is valid
-
+                
+                
     counter = 0
     for link in all_links:
-        uClient = uReq(all_links[counter])
+       # print(all_links[counter])
+        url = all_links[counter]
+        url = urllib.parse.urlsplit(url)
+        url = list(url)
+        url[2] = urllib.parse.quote(url[2])
+        url = urllib.parse.urlunsplit(url)
+        uClient = uReq(url)
         link_html = uClient.read()
         uClient.close()
     
@@ -77,5 +103,5 @@ for my_url in urls:
             f.write(r.content)
             print("finished writing")
             
+        print(counter)
         counter+=1
-    
